79a0def1cd3aa2160598466d6db09ea9bbb18b68
artyrkozel <artyr_42@mail.ru>
2021-03-28 16:10:13 +0300

    Updates

---
+++ precache-manifest.80c6fc4bc1c846ff816660d5cf6ef6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d8e4611c1f270b5267aa59052f935d",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b1fcf210fac8023b64a5",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "10335fa8e16e252a7a9a",
+    "url": "/todolist/static/js/2.6e6559c3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.6e6559c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1fcf210fac8023b64a5",
+    "url": "/todolist/static/js/main.6deeb198.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
